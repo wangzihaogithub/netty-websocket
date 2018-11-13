851c42ab44dc73b6ccb71aa2a02182bdd31a619b
王子豪 <zhwang@kunfangtech.cn>
2018-11-13 16:31:58 +0800

    1.初始化项目

---
+++ src/main/java/com/github/netty/websocket/WebSocketTaskFactory.java
@@ -0,0 +1,135 @@
+package com.github.netty.websocket;
+
+import com.github.netty.core.util.AbstractRecycler;
+import com.github.netty.core.util.Recyclable;
+import com.github.netty.core.util.TypeUtil;
+import com.github.netty.servlet.ServletContext;
+import com.github.netty.servlet.handler.TaskFactory;
+import com.github.netty.springboot.NettyProperties;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.websocketx.*;
+
+import javax.websocket.MessageHandler;
+import javax.websocket.PongMessage;
+import java.nio.ByteBuffer;
+import java.util.Set;
+
+/**
+ * websocket任务工厂
+ * @author 84215
+ */
+public class WebSocketTaskFactory implements TaskFactory {
+
+    private static final AbstractRecycler<WebsocketTask> RECYCLER = new AbstractRecycler<WebsocketTask>() {
+        @Override
+        protected WebsocketTask newInstance() {
+            return new WebsocketTask();
+        }
+    };
+
+    private TaskFactory parent;
+
+    WebSocketTaskFactory(TaskFactory parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public Runnable newTask(ServletContext servletContext, NettyProperties config, ChannelHandlerContext channelHandlerContext, Object o) {
+        if(o instanceof WebSocketFrame) {
+            WebsocketTask task = RECYCLER.getInstance();
+            task.context = channelHandlerContext;
+            task.frame = (WebSocketFrame) o;
+            return task;
+        }
+
+        if(parent != null){
+            return parent.newTask(servletContext,config,channelHandlerContext,o);
+        }
+        throw new IllegalStateException("不支持的类型");
+    }
+
+
+    /**
+     * Websocket任务
+     */
+    public static class WebsocketTask implements Runnable,Recyclable {
+        private ChannelHandlerContext context;
+        private WebSocketFrame frame;
+
+        @Override
+        public void run() {
+            try {
+                WebSocketSession wsSession = WebSocketSession.getSession(context.channel());
+                if (wsSession == null) {
+                    return;
+                }
+
+                // 关闭消息
+                if (frame instanceof CloseWebSocketFrame) {
+                    wsSession.getWebSocketServerHandshaker().close(context.channel(), (CloseWebSocketFrame) frame.retain());
+                    return;
+                }
+
+                // Ping消息
+                if (frame instanceof PingWebSocketFrame) {
+                    ByteBuffer request = frame.content().nioBuffer();
+                    onWebsocketMessage(wsSession, frame, (PongMessage) () -> request);
+                    return;
+                }
+
+                // 二进制消息
+                if (frame instanceof BinaryWebSocketFrame) {
+                    onWebsocketMessage(wsSession, frame, frame.content().nioBuffer());
+                    return;
+                }
+
+                // 字符串消息
+                if (frame instanceof TextWebSocketFrame) {
+                    onWebsocketMessage(wsSession, frame, ((TextWebSocketFrame) frame).text());
+                }
+            }finally {
+                WebsocketTask.this.recycle();
+            }
+        }
+
+        private void onWebsocketMessage(WebSocketSession wsSession, WebSocketFrame frame, Object message){
+            Class messageType = message.getClass();
+
+            Set<MessageHandler> messageHandlers = wsSession.getMessageHandlers();
+            for(MessageHandler handler : messageHandlers){
+                if(handler instanceof MessageHandler.Partial){
+                    MessageHandler.Partial<Object> partial =((MessageHandler.Partial<Object>) handler);
+                    TypeUtil.TypeResult typeResult = TypeUtil.getGenericType(MessageHandler.Partial.class, partial.getClass());
+                    if(typeResult == null
+                            || typeResult.getClazz() == Object.class
+                            || typeResult.getClazz().isAssignableFrom(messageType)){
+                        partial.onMessage(message,frame.isFinalFragment());
+                    }
+                    continue;
+                }
+
+                if(handler instanceof MessageHandler.Whole){
+                    MessageHandler.Whole<Object> whole =((MessageHandler.Whole<Object>) handler);
+                    TypeUtil.TypeResult typeResult = TypeUtil.getGenericType(MessageHandler.Whole.class,whole.getClass());
+                    if(typeResult == null
+                            || typeResult.getClazz() == Object.class
+                            || typeResult.getClazz().isAssignableFrom(messageType)){
+                        whole.onMessage(message);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void recycle() {
+            if(context instanceof Recyclable) {
+                ((Recyclable) context).recycle();
+            }
+            context = null;
+            frame = null;
+        }
+
+    }
+
+
+}
